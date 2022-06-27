cc099dd2501703ddc8d0aab56de220ab26647e58
deehood <dee.hood@gmail.com>
2022-06-27 16:58:37 +0100

    7.5, 7.6: anecdotes and hooks step2 and step3

--- routed-anecdotes/src/App.js
+++ routed-anecdotes/src/App.js
@@ -86,17 +86,20 @@ const CreateNew = (props) => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-
     props.addNew({
       content: content.value,
       author: author.value,
       info: info.value,
       votes: 0,
     });
-
     navigate("/");
   };
 
+  const handleReset = () => {
+    content.onReset();
+    author.onReset();
+    info.onReset();
+  };
   return (
     <div>
       <h2>create a new anecdote</h2>
@@ -113,7 +116,10 @@ const CreateNew = (props) => {
           url for more info
           <input {...info} />
         </div>
-        <button>create</button>
+        <button type="submit">create</button>
+        <button type="button" onClick={handleReset}>
+          reset
+        </button>
       </form>
     </div>
   );
