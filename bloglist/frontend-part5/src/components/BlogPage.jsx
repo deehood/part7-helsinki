f78db185fbb416c479d56f84750adf10b15a6db8
deehood <dee.hood@gmail.com>
2022-07-07 16:21:27 +0100

    regex and validation for blog url

--- bloglist/frontend-part5/src/components/BlogPage.jsx
+++ bloglist/frontend-part5/src/components/BlogPage.jsx
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 import { incrementLikes, setSelectorBlogs } from "../reducers/blogReducer";
 import { useEffect } from "react";
 import { getAllBlogs } from "../reducers/blogReducer";
+import webUrl from "../utils/regex-weburl";
 
 const BlogPage = () => {
   // const navigate = useNavigate;
@@ -32,9 +33,30 @@ const BlogPage = () => {
           blog.id === id ? (
             <div key={blog.id}>
               <h2> {blog.title}</h2>
-              <a href={`${blog.url}`} target="_blank" rel="noreferrer">
-                {blog.url}
-              </a>
+              {/* matches url regex */}
+              {webUrl.test(blog.url) ? (
+                // adds https if valid url without http
+                blog.url.indexOf("http") === 0 ? (
+                  <a href={`${blog.url}`} target="_blank" rel="noreferrer">
+                    {blog.url}
+                  </a>
+                ) : (
+                  <a
+                    href={`https://${blog.url}`}
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    {blog.url}
+                  </a>
+                )
+              ) : (
+                <span>
+                  {blog.url}
+                  <span style={{ color: "darkred", marginLeft: "1rem" }}>
+                    (non valid url)
+                  </span>
+                </span>
+              )}
               <p>
                 {blog.likes} likes{" "}
                 <button id="button-like" onClick={handleLikes}>
